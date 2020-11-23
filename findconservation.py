59d1db7bccffd9f3cf9dffe2bec21cfbbef3185b
Wenxuan Li <s2127878@bioinfmsc5.bio.ed.ac.uk>
2020-11-23 20:05:58 +0000

    findconservation.py 1st update:recover lots of bug,make it run successfully

--- findconservation.py
+++ findconservation.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 prot_number = os.popen('grep -c ">" protset_1p.fa').readlines()
 number_p = prot_number[0].replace('\n','')
-if number_p >= 250:
+if int(number_p) >= 250:
 	print("The number of sequence is over or equal 250, you need to select one protein, and we will find the 250 sequences which are most similar with your input")
 	
 	os.system('clustalo -i protset_1p.fa -t Protein --distmat-out=protmatrix.txt --full')
@@ -22,8 +22,8 @@ if number_p >= 250:
 	protmatrix_modify.index= header 
 	protmatrix_modify.drop(columns=0,inplace=True)
 	protmatrix_modify.columns= header 
-	print("After modifying, this is Pairwise distance matrix!\n",table)
-	print("This is the list of proteins we found",header)
+	print("After modifying, this is Pairwise distance matrix!\n",protmatrix_modify)
+	print("This is the list of proteins we found\n",header)
 	favor_prot=input("which protein you get interested with,we'll find the 250 sequences which are most similar with your input")
 	protmatrix_modify.sort_values([favor_prot.upper()],ascending=True,inplace=True)
 	favor_prot250 = list(table.index)[:250]
@@ -32,7 +32,7 @@ if number_p >= 250:
 	favor_prot250_w.write(f"{favor_prot250}")
 	favor_prot250_w.close()
 	subprocess.call("/localdisk/data/BPSM/Assignment2/pullseq -i protset_1p.fa -n favor_prot250.text > protset_2p.fa",shell=True)
-	os.system('clustalo -i protset_2p.fa -t Protein -o protset_toplotcon.fa
+	os.system('clustalo -i protset_2p.fa -t Protein -o protset_toplotcon.fa')
 	os.system('plotcon -sequences protset_toplotcon.fa -winsize 4 -graph')
 else:
 	os.system('clustalo -i protset_1p.fa -t Protein --distmat-out=protmatrix.txt --full -o protset_toplotcon.fa')
@@ -47,7 +47,7 @@ else:
 	protmatrix_modify.index= header 
 	protmatrix_modify.drop(columns=0,inplace=True)
 	protmatrix_modify.columns= header 
-	print("After modifying, this is Pairwise distance matrix!\n",table)
-	print("This is the list of proteins we found",header)
-	os.system('plotcon -sequences protset_toplotcon.fa -winsize 4 -graph ')
+	print("After modifying, this is Pairwise distance matrix!\n",protmatrix_modify)
+	print("This is the list of proteins we found\n",header)
+	os.system('plotcon -sequences protset_toplotcon.fa -winsize 4 -graph x11')
 	
