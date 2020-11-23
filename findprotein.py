860815809e2e1670c0cacb6a0fa5bbdd98727e09
Wenxuan Li <s2127878@bioinfmsc5.bio.ed.ac.uk>
2020-11-23 19:15:13 +0000

    findprotein.py 2th update recover to search partial sequence

--- findprotein.py
+++ findprotein.py
@@ -5,7 +5,7 @@ import os
 organism_input = input("which species you want to search: ")
 protein_input = input("which protein you want to search: ")
 try:
-	os.system(f'esearch -db protein -query "{organism_input}[organism] AND {protein_input}[PROT] NOT Partial" | efetch -db protein -format fasta > rawprotset.fa')
+	os.system(f'esearch -db protein -query "{organism_input}[organism] AND {protein_input}[PROT]" | efetch -db protein -format fasta > rawprotset.fa')
 	print("try to looking for the ",protein_input," of ",organism_input)
 except:
 	print("There is something wrong, maybe you should review what you type")
